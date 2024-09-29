3658fb62b2d06033e9860f9bc8cd2e25be329a55
minarhossain <codewithmnr@gmail.com>
2024-09-29 20:35:52 +0600

    comments updated

--- src/posts/PostActions.jsx
+++ src/posts/PostActions.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
-const PostActions = () => {
+import like from "../assets/icons/like.svg";
+import comment from "../assets/icons/comment.svg";
+import share from "../assets/icons/share.svg";
+const PostActions = ({ postId, commentCount }) => {
   return (
-    <div>
-      <h1>Post Actions</h1>
+    <div className="flex items-center justify-between py-6 lg:px-10 lg:py-8">
+      <button className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm">
+        <img src={like} alt="Like" />
+        <span>Like</span>
+      </button>
+
+      <button className="icon-btn space-x-2 px-6 py-3 text-xs lg:px-12 lg:text-sm">
+        <img src={comment} alt="Comment" />
+        <span>Comment({commentCount ?? 0})</span>
+      </button>
+
+      <button className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm">
+        <img src={share} alt="Share" />
+        <span>Share</span>
+      </button>
     </div>
   );
 };
--- src/posts/PostCard.jsx
+++ src/posts/PostCard.jsx
@@ -9,8 +9,8 @@ const PostCard = ({ post }) => {
     <article className="card mt-6 lg:mt-8">
       <PostHeader post={post} />
       <PostBody poster={post?.image} content={post?.content} />
-      <PostActions />
-      <PostComments />
+      <PostActions postId={post?.id} commentCount={post?.comments?.length} />
+      <PostComments post={post} />
     </article>
   );
 };
